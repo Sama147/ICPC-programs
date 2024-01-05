268131cba2655c20b4bee0f369db35d216bf52e8
Sama Ashraf <60572513+Sama147@users.noreply.github.com>
2024-01-05 03:52:18 +0200

    Add files via upload

---
+++ Nested_loops_pyramid.java
@@ -0,0 +1,16 @@
+
+public class Nested_loops_pyramid 
+{
+	public static void main(String[] args)
+	{
+		for(int i = 4; i > 0 ; i--)
+		{
+			for(int j = 0; i < 4 ; i++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("&");
+		}	
+		
+	}
+}
